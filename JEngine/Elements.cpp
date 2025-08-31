8e5c6b4b3497faca8fd9e1ce529acd18e4df4544
upthagrg <guglue4@gmail.com>
2025-08-31 13:01:30 -0700

    Test Updated

--- JEngine/Elements.cpp
+++ JEngine/Elements.cpp
@@ -9,19 +9,42 @@
 
 int main() {
    /// Xeon::XeonSetupWizard();
-    //string testJASON = "{\"name\":\"John\", \"age\":30, \"car\":null, \"obj\":{\"name2\":\"Adam\" \"Ob2\":{\"n3\":\"bill\"}},\"Sex\":M}";
-    //string testJASON2 = "{\"name\":\"John\", \"age\":30, \"car\":null, \"obj\":{\"name2\":\"Adam\" \"Ob2\":{\"n3\":\"bill\"}}}";
-    //string testJASON3 = "{\"name\":\"John\", \"age\":30, \"car\":null, \"PetNameArray\":[\"Jax\",\"Dakota\",\"Nova\"],\"Sex\":M}";
-    //string testJASON4 = "{\"name\":\"John\", \"age\":30, \"car\":null, \"PetNameArray\":[\"Jax\",\"Dakota\",\"Nova\"]}";
-    //O2::JSONObject myobj;
+    string testjason = "{\"name\":\"john\", \"age\":30, \"car\":null, \"obj\":{\"name2\":\"adam\" \"ob2\":{\"n3\":\"bill\"}},\"sex\":m}";
+    string testjason2 = "{\"name\":\"john\", \"age\":30, \"car\":null, \"obj\":{\"name2\":\"adam\" \"ob2\":{\"n3\":\"bill\"}}}";
+    string testjason3 = "{\"name\":\"john\", \"age\":30, \"car\":null, \"petnamearray\":[\"jax\",\"dakota\",\"nova\"],\"sex\":m}";
+    string testjason4 = "{\"name\":\"john\", \"age\":30, \"car\":null, \"petnamearray\":[\"jax\",\"dakota\",\"nova\"]}";
+    O2::JSONObject myobj;
 
-    //myobj.Parse(testJASON);
-    //MyBase.Display(testJASON);
-    //MyBase.Display(myobj.Stringify());
+    myobj.Parse(testjason);
+    MyBase.Display(testjason);
+    MyBase.Display(myobj.Stringify());
+    MyBase.Display("\n");
+
+    myobj.Parse(testjason2);
+    MyBase.Display(testjason2);
+    MyBase.Display(myobj.Stringify());
+    MyBase.Display("\n");
+
+
+    myobj.Parse(testjason3);
+    MyBase.Display(testjason3);
+    MyBase.Display(myobj.Stringify());
+    MyBase.Display("\n");
+
+
+    myobj.Parse(testjason4);
+    MyBase.Display(testjason4);
+    MyBase.Display(myobj.Stringify());
+    MyBase.Display("\n");
+
+
+
     HFile file1("C:\\ServerFiles\\TEST.txt");
     MyBase.Display("File 1 contents");
     MyBase.Display(file1.Data());
     MyBase.Display(file1.DataString());
+    MyBase.Display("\n");
+
 
     HFile file2 = file1;
     MyBase.Display("File 2 contents");
